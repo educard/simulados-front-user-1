f62cf94771b5240cac4fcd31259e2573d8f7ff68
Eduardo Cardoso Ferreira <eduardocardosoferreira@MacBook-Air-de-Eduardo.local>
2019-11-04 20:53:29 -0300

    Added Logout feature

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,20 +2,30 @@ import React from 'react'
 
 import Logo from '../Logo'
 import Icon from '../Icon'
+import Button from 'components/Button'
 import media from 'utils/media'
 import { connect } from 'react-redux'
+import { logout } from 'store/user/actions'
 
 import { getUsername } from 'store/user'
 
 import './header.scss'
 
-const Header = ({ username = '', location }) =>
+const onPressEnter = () => {
+  console.log('lalalalalala')
+
+  localStorage.clear()
+  window.location.reload()
+}
+
+const Header = ({ username = '', location, logout }) =>
   media.greaterThan.phone() ? (
     <header className="header">
       <div className="header__content">
         <Logo />
         <section className="user-section space-between-inline-m">
           <Icon name="user" height={30} width={30} />
+          <Button onClick={onPressEnter}>Logout</Button>
           <strong>{username}</strong>
         </section>
       </div>
@@ -36,6 +46,7 @@ const Header = ({ username = '', location }) =>
         />
       </span>
       <Logo small />
+
       <span style={{ width: 28 }} />
     </header>
   )
